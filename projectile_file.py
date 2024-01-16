9afa0a6946cdd371fda8b5dea480f96967b151ea
Marco <marcokot@icloud.com>
2024-01-16 10:52:26 +0100

    projectile rotation

--- projectile_file.py
+++ projectile_file.py
@@ -1,5 +1,5 @@
 import pygame
-
+import math
 class Projectile:
     def __init__(self, x, y, dx, dy):
         self.x = x
@@ -10,12 +10,22 @@ class Projectile:
         self.dx = dx
         self.dy = dy
 
+        self.image = pygame.image.load("FrostLaser.png").convert_alpha()
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+
     def move(self):
         self.x += self.dx * self.speed
         self.y += self.dy * self.speed
+        self.rect.center = (self.x, self.y)
+
+        # Calculate the angle (in degrees)
+        angle = math.degrees(math.atan2(-self.dy, self.dx))
+        self.rotated_image = pygame.transform.rotate(self.image, angle)
+        self.rect = self.rotated_image.get_rect(center=self.rect.center)
+
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.size, self.size))
+        screen.blit(self.rotated_image, self.rect)
 
     def collides_with(self, other):
         return (self.x < other.x + other.size and self.x + self.size > other.x and
